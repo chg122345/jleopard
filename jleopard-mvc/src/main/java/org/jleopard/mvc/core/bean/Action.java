55de45e38edbbe8a315495250cdd0d0a3d5d33be
Chen_9g <502622717@qq.com>
2018-10-09 14:06:59 +0800

    修改空值过滤，去除事物回滚异常

---
+++ jleopard-mvc/src/main/java/org/jleopard/mvc/core/bean/Action.java
@@ -0,0 +1,13 @@
+/**
+ * @author (c) 2018, Chen_9g 陈刚 (80588183@qq.com).
+ * @date 2018-10-09  上午10:22
+ *
+ * <p>
+ * Find a way for success and not make excuses for failure.
+ * </p>
+ */
+
+package org.jleopard.mvc.core.bean;
+
+public class Action {
+}
